6db1aef397d1b1d50622a1072e4b50ebe06a8c6b
felipetojal <153757693+felipetojal@users.noreply.github.com>
2024-10-01 00:17:41 -0300

    Update Maracuja.java

--- cataFutas/src/Frutas/Maracuja.java
+++ cataFutas/src/Frutas/Maracuja.java
@@ -1,1 +1,30 @@
+package Frutas;
+
+import Jogador.*;
+/**
+ * Implementação da fruta Maracuja
+ * É a fruta ouroo efeito dela é aumentar os pontos de vitória
+ */
+public class Maracuja extends Fruta {
+	
+	/**
+	 * O construtor define a fruta sendo ouro e não bichada
+	 * @author felipeTojal
+	 */
+	public Maracuja() {
+		this.bichada = false;
+		this.ouro = true;
+	}
+	
+	/**
+	 * O efeito causado pelo maracujá é que ele fornece um ponto de vitória ao jogador, por ser uma fruta ouro
+	 * @author felipeTojal
+	 */
+	@Override
+	public void causarEfeito(Competidor competidor) {
+		competidor.incrementaPontosVitoria(); // Efeito específico do Maracuja
+		super.causarEfeito(competidor); // Chama o efeito comum definido na classe Fruta
+	}
+
+}
 
