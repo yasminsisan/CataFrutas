c78af82bf730ed65c71f005ab8125da9b0b1944a
felipetojal <153757693+felipetojal@users.noreply.github.com>
2024-10-01 00:16:38 -0300

    Update Fruta.java

--- cataFutas/src/Frutas/Fruta.java
+++ cataFutas/src/Frutas/Fruta.java
@@ -1,1 +1,35 @@
+package Frutas;
 
+import Jogador.*;
+
+/**
+ * Classe Fruta que servirá como base para os tipos de frutas do jogo
+ */
+public abstract class Fruta {
+	
+	protected boolean ouro;
+	protected boolean bichada;
+	
+	public Fruta() {
+		this.bichada = false;
+		this.ouro = false;
+	}
+	
+	/**
+	 * Esse método serve para alterar o atributo 'bichada' para true e indicar que a fruta está bichada
+	 * @author felipeTojal
+	 */
+	public void frutaBichada() {
+		this.bichada = true;
+	}
+	/**
+	 * O método causar efeito representa o efeito que uma fruta terá no competidor
+	 * Cada fruta causa um efeito diferente, portanto cada uma implementará esse método de maneira separada
+	 * O único efeito geral das frutas é fornecer poder de força 1 ao competidor
+	 * @author felipeTojal
+	 */
+	public void causarEfeito(Competidor competidor) {
+		competidor.incrementaPoderForça();
+	}
+	
+}
