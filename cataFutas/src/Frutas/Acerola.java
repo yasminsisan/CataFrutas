46418fda361c139cf3a7ee8305ae1cb53c2fc69d
felipetojal <153757693+felipetojal@users.noreply.github.com>
2024-10-01 00:22:55 -0300

    Create Acerola.java

---
+++ cataFutas/src/Frutas/Acerola.java
@@ -0,0 +1,17 @@
+package Frutas;
+
+/**
+ * Implementação da fruta Acerola
+ * O único efeito causado pela Acerola é o efeito comum a todas as frutas
+ */
+public class Acerola extends Fruta {
+	
+	/**
+	 * O construtor de Acerola chama o construtor da super classe
+	 * @author felipeTojal
+	 */
+	public Acerola() {
+		super();
+	}
+
+}
