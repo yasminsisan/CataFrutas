78a2351adbaba051cbd5ddb62f7966b457f6a243
Paulo607 <paulohenriquecarvalho607@gmail.com>
2024-10-01 22:05:15 -0300

    Update aceroleira.java

--- cataFutas/src/cataFrutas/aceroleira.java
+++ cataFutas/src/cataFrutas/aceroleira.java
@@ -2,11 +2,7 @@ package cataFrutas;
 
 public class aceroleira extends arvore {
 
-   /**
-	 * O construtor de aceroleira chama o construtor da super classe
-	 */
-	public aceroleira() {
-		super();
-	}
-
+ public Fruta gerarFruta() {
+        return new Acerola();
+    }
 }
