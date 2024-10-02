914050ed926dbab90a0f420d4b7866684a7c9369
Paulo607 <paulohenriquecarvalho607@gmail.com>
2024-10-01 21:17:25 -0300

    Update amoreira.java

--- cataFutas/src/cataFrutas/amoreira.java
+++ cataFutas/src/cataFrutas/amoreira.java
@@ -2,4 +2,11 @@ package cataFrutas;
 
 public class amoreira extends arvore {
 
+   /**
+	 * O construtor de amoreira chama o construtor da super classe
+	 */
+	public amoreira() {
+		super();
+	}
+
 }
