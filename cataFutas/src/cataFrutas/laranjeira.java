57ea6c501a4fab1a7b05ff11de5aca39b5371a08
Paulo607 <paulohenriquecarvalho607@gmail.com>
2024-10-01 22:07:13 -0300

    Update laranjeira.java

--- cataFutas/src/cataFrutas/laranjeira.java
+++ cataFutas/src/cataFrutas/laranjeira.java
@@ -2,11 +2,7 @@ package cataFrutas;
 
 public class laranjeira extends arvore {
 
-   /**
-	 * O construtor de laranjeira chama o construtor da super classe
-	 */
-	public laranjeira() {
-		super();
-	}
-
+ public Fruta gerarFruta() {
+        return new Laranja();
+    }
 }
