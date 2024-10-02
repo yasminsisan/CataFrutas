f5797ffe5fbcada0375c66431e1f3bb3491c925f
Paulo607 <paulohenriquecarvalho607@gmail.com>
2024-10-01 21:19:38 -0300

    Update goiabeira.java

--- cataFutas/src/cataFrutas/goiabeira.java
+++ cataFutas/src/cataFrutas/goiabeira.java
@@ -2,4 +2,11 @@ package cataFrutas;
 
 public class goiabeira extends arvore {
 
+   /**
+	 * O construtor de goiabeira chama o construtor da super classe
+	 */
+	public goiabeira() {
+		super();
+	}
+
 }
