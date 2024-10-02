ca3e1aa526df4aa78a3d39f1692edc0b14456848
Paulo607 <paulohenriquecarvalho607@gmail.com>
2024-10-01 22:06:33 -0300

    Update coqueiro.java

--- cataFutas/src/cataFrutas/coqueiro.java
+++ cataFutas/src/cataFrutas/coqueiro.java
@@ -2,11 +2,7 @@ package cataFrutas;
 
 public class coqueiro extends arvore {
 
-   /**
-	 * O construtor de coqueiro chama o construtor da super classe
-	 */
-	public coqueiro() {
-		super();
-	}
-
+ public Fruta gerarFruta() {
+        return new Coco();
+    }
 }
