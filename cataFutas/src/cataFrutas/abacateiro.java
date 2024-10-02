f383accca4e1f29227aa736e34b8ecc4d21618e8
Paulo607 <paulohenriquecarvalho607@gmail.com>
2024-10-01 21:14:44 -0300

    Update abacateiro.java

--- cataFutas/src/cataFrutas/abacateiro.java
+++ cataFutas/src/cataFrutas/abacateiro.java
@@ -2,4 +2,11 @@ package cataFrutas;
 
 public class abacateiro extends arvore {
 
+  /**
+	 * O construtor de acabacateiro chama o construtor da super classe
+	 */
+	public acabacateiro() {
+		super();
+	}
+
 }
