a507e1e7120ba8225bcdbad465146290b2b30745
Pedro Thalles <131317645+Pedro-Thalles@users.noreply.github.com>
2024-10-10 12:31:14 -0300

    Update abacateiro.java

--- cataFutas/src/Arvores/abacateiro.java
+++ cataFutas/src/Arvores/abacateiro.java
@@ -1,8 +1,16 @@
 package Arvores;
 import Frutas.*;
+/**
+ * Classe que representa o Abacateiro
+ * @author Pedro Thalles Lima Ricarte
+ */
 public class abacateiro extends arvore {
-
+/**
+ * Método que cria uma instância da classe Abacate
+ * @return instância da classe Abacate
+ */
  public Fruta gerarFruta() {
         return new Abacate();
     }
 }
+
